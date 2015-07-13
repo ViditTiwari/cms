9734450d5172a7f5b07000a63d2ee808581d0a48
Vidit Tiwari <vidittiwari95@gmail.com>
2015-07-14 02:53:25 +0530

    header

---
+++ header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CVS</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300|Open+Sans:300italic,400italic,600italic,400,300,600,700' rel='stylesheet' type='text/css'>
+<!--//webfont-->
+<!--Animation-->
+<script src="js/wow.min.js"></script>
+<link href="css/animate.css" rel='stylesheet' type='text/css' />
+<script>
+	new WOW().init();
+</script>
+
+</head>
+<body>
+	<div class="header box css3-shadow" id="head"">
+ 	<div class="container">
+	  <div class="header-top">
+	        <div class="logo wow bounceInLeft" data-wow-delay="0.4s">
+				<a href="index.html"><img src="images/logo.png" alt=""/></a>
+			 </div>
+		     <div class="h_menu4"><!-- start h_menu4 -->
+				<a class="toggleMenu" href="#">Menu</a>
+				<ul class="nav">
+					<li class="active"><a class="scroll" href="index.html">Home</a></li>
+						<li><a href="#about" class="scroll">About</a></li>
+						<li><a href="portfolio.html">Portfolio</a>
+						<li><a href="feature.html">Features</a>
+							<ul>
+								<li><a href="feature.html">Typography</a></li>
+								<li><a href="feature.html">Shortcodes</a></li>
+								<li><a href="feature.html">Columns</a></li>
+								<li><a href="feature.html">Pricing plans</a></li>
+								<li><a href="feature.html">Alerts</a></li>
+								<li><a href="feature.html">Services</a></li>
+							</ul>
+						</li>
+					<li><a href="#blog" class="scroll">Blog</a></li>
+					<li><a href="contact.html">Contact</a></li>
+
+				</ul>
+				<script type="text/javascript" src="js/nav.js"></script>
+			</div><!-- end h_menu4 -->
+			<div class="social_icons wow bounceInRight" data-wow-delay="0.4s">
+					<ul>
+						<li><a href="#"><i class="facebook"></i></a></li>
+						<li><a href="#"><i class="twitter"></i></a></li>
+						<li><a href="#"><i class="rss"></i></a></li>
+						<li><a href="#"><i class="viemo"></i></a></li>
+						<li><a href="#"><i class="youtube"></i></a></li>
+						<li><a href="#"><i class="linkedin"></i></a></li>
+					</ul>
+				</div>
+
+			<div class="clearfix"> </div>
+			
+		  </div><!-- end header_main4 -->
+	</div>
+</div>
