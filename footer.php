42d24f080a90c4897a332992e8d84e6e4419fec4
Vidit Tiwari <vidittiwari95@gmail.com>
2015-07-15 01:25:10 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,122 @@
+			<!--/start-footer-section-->
+			<div class="footer-section">
+				<div class="container">
+					<div class="footer-grids wow bounceIn animated" data-wow-delay="0.4s">
+						<div class="col-md-3 footer-grid">
+						<h4>About studeon</h4>
+						<div class="border2"></div>
+						  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id arcu neque, at convallis est felis.</p>
+						  <p class="sub">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id arcu neque, at convallis est felis.</p>
+						</div>
+						<div class="col-md-3 footer-grid tags">
+								<h4>the tags</h4>
+								<div class="border2"></div>
+							<ul class="tag">
+								<li><a href="#">Awesome</a></li>
+								<li><a href="#">Strategy</a></li>
+								<li><a href="#">Photography</a></li>
+								<li><a href="#">Development</a></li>
+								<li><a href="#">Css</a></li>
+								<li><a href="#">photoshop</a></li>
+								<li><a href="#">Html</a></li>
+								<li><a href="#">Awesome</a></li>
+								<li><a href="#">Strategy</a></li>
+								<li><a href="#">Photoshop</a></li>
+								<li><a href="#">Html</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3 footer-grid tweet">
+								<h4>latest tweets</h4>
+								<div class="border2"></div>
+								<div class="icon-3-square">
+										<a href="#"><i class="square-3"></i></a>
+									</div>
+							<div class="icon-text">
+								<p>I like this awesome freebie. Check it out<a href="#"> @http://t.co/9vslJFpW </a></p>
+								<h5>About 15 mins</h5>
+							</div>
+								<div class="clearfix"></div>
+								<div class="icon-3-square">
+										<a href="#"><i class="square-3"></i></a>
+								</div>
+								<div class="icon-text">
+								    <p>I like this awesome freebie. Check it out<a href="#"> @http://t.co/9vslJFpW </a></p>
+									<h5>About 15 mins</h5>
+								</div>
+								<div class="clearfix"></div>
+						</div>
+						<div class="col-md-3 footer-grid flickr">
+								<h4>flickr feed</h4>
+								<div class="border2"></div>
+								<div class="flickr-grids">
+										<div class="flickr-grid">
+											<a href="#"><img src="images/f1.png" alt=" " title="CEO" /></a>
+										</div>
+										<div class="flickr-grid">
+											<a href="#"><img src="images/f2.png" alt=" " title="GM" /></a>
+										</div>
+										<div class="flickr-grid">
+											<a href="#"><img src="images/f3.png" alt=" " title="CEO" /></a>
+										</div>
+										<div class="clearfix"> </div>
+										
+										<div class="flickr-grid">
+											<a href="#"><img src="images/f4.png" alt=" " title="GM" /></a>
+										</div>
+										<div class="flickr-grid">
+											<a href="#"><img src="images/f3.png" alt=" " title="CEO" /></a>
+										</div>
+										<div class="flickr-grid">
+											<a href="#"><img src="images/f2.png" alt=" " title="GM" /></a>
+										</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+			</div>
+		</div>
+	<!--//end-footer-section-->
+			<!--/start-copyright-section-->
+				<div class="copyright">
+					<div class="container">
+					<div class="logo2  wow bounceInLeft" data-wow-delay="0.4s"><a href="index.html"><img src="images/logo2.png" alt=""/ title="logo" /></a></div>
+					<p class="write  wow bounceInRight" data-wow-delay="0.4s">Copyright &copy; 2015  All rights  Reserved | Template by &nbsp;<a href="#">CVS</a></p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			
+			<!--//end-copyright-section-->
+			<!--start-smoth-scrolling-->
+			<script type="text/javascript" src="js/move-top.js"></script>
+			<script type="text/javascript" src="js/easing.js"></script>
+			<script type="text/javascript">
+								jQuery(document).ready(function($) {
+									$(".scroll").click(function(event){		
+										event.preventDefault();
+										$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+									});
+								});
+								</script>
+							<!--start-smoth-scrolling-->
+						<script type="text/javascript">
+									$(document).ready(function() {
+										/*
+										var defaults = {
+								  			containerID: 'toTop', // fading element id
+											containerHoverID: 'toTopHover', // fading element hover id
+											scrollSpeed: 1200,
+											easingType: 'linear' 
+								 		};
+										*/
+										
+										$().UItoTop({ easingType: 'easeOutQuart' });
+										
+									});
+								</script>
+		<a href="#head" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+			
+</body>
+</html>
