0893704440afe85918c698e5bde47f6363bcd761
Vidit Tiwari <vidittiwari95@gmail.com>
2015-07-30 22:44:34 +0530

    previous-design

---
+++ previous/header.php
@@ -0,0 +1,54 @@
+<?php
+require_once('includes/config_new.php'); 
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>College of Vocational Studies</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="css/layout.css" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+<!-- liteAccordion is Homepage Only -->
+<link rel="stylesheet" href="css/liteaccordion.css" type="text/css" />
+</head>
+<body id="top">
+<div class="wrapper row1">
+  <div id="header" class="clear">
+    <div class="fl_left">
+      <h1><a href="index.html">College of Vocational Studies</a></h1>
+      <p class ="sub-heading">University of Delhi</p>
+    </div>
+    <form action="#" method="post" id="login">
+      <fieldset>
+        <legend>Student Login</legend>
+        <input type="password" />
+        <input type="text" />
+        <div id="forgot">Need <a href="#">Help ?</a> or <a href="#">Forgot Your Details ?</a></div>
+        <input type="image" src="images/sign_in.gif" id="signin" alt="Sign In" />
+      </fieldset>
+    </form>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper row2">
+  <div id="topnav">
+    <ul>
+      <li class="active"><a href="index.html">Homepage</a></li>
+      <li><a href="pages/style-demo.html">Administration</a></li>
+      <li><a href="pages/full-width.html">Facilities</a></li>
+      <li><a href="#">Academics</a>
+        <ul>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+        </ul>
+      </li>
+      <li><a href="pages/3-columns.html">Courses</a></li>
+      <li class="last"><a href="pages/gallery.html">Contact Us</a></li>
+    </ul>
+    <div  class="clear"></div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
